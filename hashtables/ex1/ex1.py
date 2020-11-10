abae94de86f71e19cc9e9657a0eca3ab4a664527
BaoPham92 <baopham92@yahoo.com>
2020-11-09 23:54:32 -0500

    ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,13 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    storage = {}
+    for index, weight in enumerate(weights):
+        try:
+            indexTwo = storage[limit - weight]
+        except:
+            storage[weight] = index
+        else:
+            value = ([index, indexTwo])
+            return value
 
-    return None
+# weights = [12, 6, 7, 14, 19, 3, 0, 25, 40]
+# get_indices_of_item_weights(weights, 9, 7)
